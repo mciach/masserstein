532207ec78b6a62960cbdee3e371438a2d5e4f41
Ciach <m_ciach@student.uw.edu.pl>
2018-10-21 17:42:28 +0200

    Denoising debug

--- WSDistance.py
+++ WSDistance.py
@@ -91,7 +91,7 @@ if norm:
 
 if thr < 1:
     order1 = sorted([x for x in enumerate((l[1] for l in sp1))], key=lambda y: y[1])  # ordering of intensities
-    cmsm1 = np.cumsum(order1)  # cumsum of ordered intensities
+    cmsm1 = np.cumsum([x[1] for x in order1])  # cumsum of ordered intensities
     to_remove1 = [o[0] for o, c in zip(order1, cmsm1) if c < 1-thr]  # indices of peaks below threshold
     sp1 = [l for i, l in enumerate(sp1) if i not in to_remove1]  # denoised spectrum
 
