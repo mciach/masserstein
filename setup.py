7132bb7445d8bbbff1de17c30f42f464efa4d749
mciach <michal.ciach@um.edu.mt>
2024-11-19 13:12:04 +0100

    clean

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',  # Required
+    version='2.0.0',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -196,12 +196,12 @@ setup(
     #    ],
     #},
 
-    entry_points = {
-        'console_scripts': [
-            'WSDistance=masserstein.WSDistance:main',
-            'WSDeconv=masserstein.WSDeconv:main'
-            ]
-    }
+    #entry_points = {
+    #    'console_scripts': [
+    #        'WSDistance=masserstein.WSDistance:main',
+    #        'WSDeconv=masserstein.WSDeconv:main'
+    #        ]
+    #}
 
     # List additional URLs that are relevant to your project as a dict.
     #
